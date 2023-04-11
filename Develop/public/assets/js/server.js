ad24c5db69018776bbdfad602f998f69d7a0507e
Preston C <pchandler858@gmail.com>
2023-04-10 21:31:42 -0700

    routes set up, need to debug.

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
-let notes = [];
+// let notes = [];
+const dbPath = path.join(__dirname, "../../../db/db.json");
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../index.html"));
@@ -19,16 +21,72 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  return res.json(notes);
+  fs.readFile(dbPath, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error reading the database");
+    }
+    const notes = JSON.parse(data);
+    return res.json(notes);
+  });
 });
 
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
   newNote.id = Date.now().toString();
-  notes.push(newNote);
-  return res.json(newNote);
+
+  fs.readFile(dbPath, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error reading the database");
+    }
+
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile(dbPath, JSON.stringify(notes), "utf-8", (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send("Error writing to the database");
+      }
+
+      return res.json(newNote);
+    });
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile(dbPath, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error reading the database");
+    }
+
+    try {
+      let notes = JSON.parse(data);
+      notes = notes.filter((note) => note.id !== id);
+      fs.writeFile(dbPath, JSON.stringify(notes), "utf-8", (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send("Error writing to the database");
+        }
+
+        return res.sendStatus(200);
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send("Error parsing the database");
+    }
+  });
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
 });
+
+module.exports = app;
